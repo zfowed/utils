ba2af284b1131ab9b9aa3a9254d49b97066b9b44
zfowed <zfowed@qq.com>
2018-12-18 12:58:48 +0800

    模板引擎

---
+++ entry/template/index.js
@@ -0,0 +1,29 @@
+export default function template (template, options) {
+  const templateSettings = Object.assign({
+    escape: /<%=\s*([\s\S]+?)\s*%>/g,
+    interpolate: /<%-\s*([\s\S]+?)\s*%>/g,
+    evaluate: /<%\s*([\s\S]+?)\s*%>/g
+  }, options || {})
+  const regExp = RegExp(
+    templateSettings.escape.source + '|' +
+    templateSettings.interpolate.source + '|' +
+    templateSettings.evaluate.source + '|([\\s\\S]+?(?=' +
+    templateSettings.escape.source + '|' +
+    templateSettings.interpolate.source + '|' +
+    templateSettings.evaluate.source + ')|[\\s\\S]*$)|$', 'g')
+  let compile = `(function (obj) {obj||(obj = {});var __t,__p='';with(obj){`
+  compile += template.replace(regExp, function (match, escape, interpolate, evaluate, string) {
+    if (escape) {
+      return '__p+=String((__t=(' + escape + '))==null?\'\':__t).replace(/[&<>"\']/g, function (k) {var obj={"\\"": "&quot;","&":"&amp;","\\\'":"&#39;","<":"&lt;",">":"&gt;"};return obj[k];});'
+    } else if (interpolate) {
+      return '__p+=((__t=(' + interpolate + '))==null?\'\':__t);'
+    } else if (evaluate) {
+      return evaluate
+    } else if (string) {
+      return '__p+=' + JSON.stringify(string) + ';'
+    }
+    return ''
+  })
+  compile += '}return __p;})'
+  return eval(compile) // eslint-disable-line no-eval
+}
