e1918400c072432b2d3eb54a51041f44d57f7b4d
zfowed <zfowed@qq.com>
2018-12-18 13:52:56 +0800

    代码检查

--- entry/groupBy/index.js
+++ entry/groupBy/index.js
@@ -1,7 +1,7 @@
 export default function groupBy (collection, iteratee) {
   const group = {}
   for (let index = 0; index < collection.length; index++) {
-    const item = collection[index];
+    const item = collection[index]
     const key = typeof iteratee === 'function' ? iteratee(item, index) : item[iteratee]
     if (!group[key]) group[key] = []
     group[key].push(item)
