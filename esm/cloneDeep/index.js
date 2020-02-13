122a7ce2be93527c453f4770f77b48ffee93b30f
zfowed <zfowed@qq.com>
2020-02-13 23:04:54 +0800

    cloneDeep

--- esm/cloneDeep/index.js
+++ esm/cloneDeep/index.js
@@ -5,11 +5,12 @@
  * @param {*} obj
  * @returns
  */
-function isObj (obj) {
+function isObject (obj) {
   return (typeof obj === 'object' || typeof obj === 'function') && obj !== null
 }
 
 function cloneDeep (obj, hash = new WeakMap()) {
+  if (!isObject(obj)) return obj
   let cloneObj
   const Constructor = obj.constructor
   switch (Constructor) {
@@ -19,13 +20,16 @@ function cloneDeep (obj, hash = new WeakMap()) {
     case Date:
       cloneObj = new Constructor(obj.getTime())
       break
+    case Function:
+      cloneObj = obj
+      break
     default:
       if (hash.has(obj)) return hash.get(obj)
       cloneObj = new Constructor()
       hash.set(obj, cloneObj)
   }
   for (const key in obj) {
-    cloneObj[key] = isObj(obj[key]) ? cloneDeep(obj[key], hash) : obj[key]
+    cloneObj[key] = isObject(obj[key]) ? cloneDeep(obj[key], hash) : obj[key]
   }
   return cloneObj
 }
